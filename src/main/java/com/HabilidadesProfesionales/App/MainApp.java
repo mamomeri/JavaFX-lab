b0a3315129edf36be9ce9e8c308ce2b77d033154
mamomeri <mamomeri@espol.edu.ec>
2025-12-26 01:09:29 -0500

    feature: added the entire tutorial

--- src/main/java/com/HabilidadesProfesionales/App/MainApp.java
+++ src/main/java/com/HabilidadesProfesionales/App/MainApp.java
@@ -1,18 +1,36 @@
 package com.HabilidadesProfesionales.App;
 
+import com.HabilidadesProfesionales.Examples.Animations.Animations;
+import com.HabilidadesProfesionales.Examples.BlurModal.BlurModalExample;
 import com.HabilidadesProfesionales.Examples.ButtonExample.ButtonManualExample;
 import com.HabilidadesProfesionales.Examples.ButtonExampleFXML.ButtonFXMLExample;
+import com.HabilidadesProfesionales.Examples.CSSstyling.CSSstyling;
+import com.HabilidadesProfesionales.Examples.CommunicationBetweenControllers.CommunicationControllers;
 import com.HabilidadesProfesionales.Examples.EventHandlingSceneBuilder.EventHandlingSceneBuilderExample;
+import com.HabilidadesProfesionales.Examples.GUIFest.GUIFest;
+import com.HabilidadesProfesionales.Examples.KeyEvent.KeyEvent;
+import com.HabilidadesProfesionales.Examples.LogOutExitButton.LogOutExitButton;
 import com.HabilidadesProfesionales.Examples.LoginExampleLayoutFXML.LoginExampleLayoutFXMLExample;
+import com.HabilidadesProfesionales.Examples.MP3Player.MP3Player;
+import com.HabilidadesProfesionales.Examples.MP3Player.View.MP3PlayerView;
 import com.HabilidadesProfesionales.Examples.MVCCompleteDesing.MVCCompleteDesignExample;
+import com.HabilidadesProfesionales.Examples.MediaView.MediaView;
 import com.HabilidadesProfesionales.Examples.ScenesAndDrawingStuff.DrawingStuffExample;
 import com.HabilidadesProfesionales.Examples.Stages.StageExample;
+import com.HabilidadesProfesionales.Examples.SwitchScenes.SwitchScenes;
+import com.HabilidadesProfesionales.Examples.WebBrowser.WebBrowser;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.util.Objects;
 
@@ -21,9 +39,21 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) {
 
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            onLogout(stage);
+        });
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent arg0) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
         Navigator.init(stage);
 
-        // === ELIGE QUÉ PROBAR (UNA A LA VEZ) ===
+        // === ELIGE QUÃ‰ PROBAR (UNA A LA VEZ) ===
 
         /*
 
@@ -33,18 +63,29 @@ public class MainApp extends Application {
             WindowSpec.fixed(width, height) //sin capacidad de maximizar
             WindowSpec.maximized()
             WindowSpec.fullscreen()
-            WindowSpec.of(width, height) // ✅ TAMAÑO CUSTOM con control de resizable
+            WindowSpec.of(width, height) // âœ… TAMAÃ‘O CUSTOM con control de resizable
         * */
         //TUTORIAL
         //showStageExample();
         //showDrawingStuffExample();
         //showEventHandlingSceneBuilderExample();
+        //showCSSstylingExample();
+        //showSwitchEsceneExample();
+        //showBlurModalExample();
+        //showCommunicationControllersExample();
+        //showLogoutExample();
+        //showGUIFestExample();
         //ejemplos propios
-        showMVCCompleteDesignExample();
+
+        //showMVCCompleteDesignExample();
         //showLoginExampleLayoutFXMLExample();
         //showButtonManualExample();
         //showButtonFXMLExample();
-
+        //showKeyEventExample();
+        //showAnimationsExample();
+        //showMediaViewExample();
+        //showWebBrowserExample();
+        showMP3PlayerExample();
         //Usa MAVEN para obtener la ruta
         Image icono = new Image(Objects.requireNonNull(MainApp.class.getResourceAsStream(
                 "/Images/logo.png"
@@ -76,8 +117,42 @@ public class MainApp extends Application {
     private void showDrawingStuffExample(){
         Navigator.show(new DrawingStuffExample().view(), WindowSpec.of(600,600));
     }
+    private void showCSSstylingExample(){
+        Navigator.show(new CSSstyling().view(), WindowSpec.MEDIUM);
+    }
+    private void showSwitchEsceneExample(){
+        Navigator.show(new SwitchScenes().view(), WindowSpec.MEDIUM);
+    }
 
+    private void showCommunicationControllersExample(){
+        Navigator.show(new CommunicationControllers().view(), WindowSpec.LARGE);
+    }
+    private void showLogoutExample(){
+        Navigator.show(new LogOutExitButton().view(), WindowSpec.LARGE);
+    }
+    private void showGUIFestExample(){
+        Navigator.show(new GUIFest().view(), WindowSpec.LARGE);
+    }
+    private void showKeyEventExample(){
+        Navigator.show(new KeyEvent().view(), WindowSpec.LARGE);
+    }
+    private void showAnimationsExample(){
+        Navigator.show(new Animations().view(), WindowSpec.LARGE);
+    }
+    private void showMediaViewExample(){
+        Navigator.show(new MediaView().view(), WindowSpec.LARGE);
+    }
+    private void showWebBrowserExample(){
+        Navigator.show(new WebBrowser().view(), WindowSpec.LARGE);
+    }
+    private void showMP3PlayerExample(){
+        Navigator.show(new MP3Player().view(), WindowSpec.LARGE);
+    }
     //PROPIOS
+    private void showBlurModalExample(){
+        Navigator.show(new BlurModalExample().view(), WindowSpec.MEDIUM);
+    }
+
     private void showMVCCompleteDesignExample(){
         Navigator.show(new MVCCompleteDesignExample().view(), WindowSpec.of(600,600));
     }
@@ -92,8 +167,24 @@ public class MainApp extends Application {
     }
 
 
+    public void onLogout(Stage stage){
 
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("Estas a punto de desloguearte");
+        alert.setContentText("Quieres guardar antes de salir?");
+
+        if(alert.showAndWait().get() == ButtonType.OK){
+
+            System.out.println("You succesfully logged out!");
+            stage.close();
+        }
+
+
+
+    }
     public static void main(String[] args) {
         launch(args);
     }
 }
+
